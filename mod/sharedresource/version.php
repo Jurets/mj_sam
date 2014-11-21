7ff480dfd8a544bc9f897bd03c4c2337a1311043
Jurets <jurets75@gmail.com>
2014-11-21 14:58:17 +0200

    add sharedresource module

---
+++ mod/sharedresource/version.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * @author  Piers Harding  piers@catalyst.net.nz
+ * @reauthor  Valery Fremaux  valery.fremaux@gmail.com
+ * @contributor  Wafa Adham  admin@adham.ps
+ * @version 0.0.1
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License, mod/taoresource is a work derived from Moodle mod/resoruce
+ * @package sharedresource
+ *
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+//  Code fragment to define the module version etc.
+//  This fragment is called by /admin/index.php
+////////////////////////////////////////////////////////////////////////////////
+
+defined('MOODLE_INTERNAL') || die();
+
+$module->version   = 2013050100;       // The current module version (Date: YYYYMMDDXX)
+$module->requires  = 2012062500;       // Requires this Moodle version
+$module->component = 'mod_sharedresource';     // Full name of the plugin (used for diagnostics)
+$module->cron      = 0;
+$module->maturity = MATURITY_BETA;     // Full name of the plugin (used for diagnostics)
+$module->release = '2.4.0 (Build 2013031800)';     // Full name of the plugin (used for diagnostics)
+
