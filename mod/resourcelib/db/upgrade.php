17faaab1fd5c8ce1b8099ccc0934f086232f6c24
Jurets <jurets75@gmail.com>
2014-12-12 12:32:59 +0200

    * correct version, xml, upgrade

--- mod/resourcelib/db/upgrade.php
+++ mod/resourcelib/db/upgrade.php
@@ -39,7 +39,7 @@ defined('MOODLE_INTERNAL') || die();
  */
 function xmldb_resourcelib_upgrade($oldversion) {
     global $DB;
-DebugBreak();
+
     $dbman = $DB->get_manager(); // Loads ddl manager and xmldb classes.
 
     /*
@@ -68,73 +68,28 @@ DebugBreak();
      *
      * First example, some fields were added to install.xml on 2007/04/01
      */
+
      $table = new xmldb_table('resourcelib');
      
-     if ($oldversion <= 2014112401) {
+     /*if ($oldversion <= 2014112401) {
          if (!$dbman->table_exists($table)) {
              $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
              $table->add_field('name', XMLDB_TYPE_TEXT, '255', null, XMLDB_NOTNULL, null, '');
              $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
              $dbman->create_table($table);
          }
-     }
+     }*/
      
-     if ($oldversion <= 2014112402) {
-
+     /*if ($oldversion <= 2014112402) {
         // Define field course to be added to resourcelib.
-        //$table = new xmldb_table('resourcelib');
         $field = new xmldb_field('course', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'id');
         // Add field course.
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
-
-        // Define field intro to be added to resourcelib.
-        //$table = new xmldb_table('resourcelib');
-        $field = new xmldb_field('intro', XMLDB_TYPE_TEXT, 'medium', null, null, null, null, 'name');
-        // Add field intro.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define field introformat to be added to resourcelib.
-        //$table = new xmldb_table('resourcelib');
-        $field = new xmldb_field('introformat', XMLDB_TYPE_INTEGER, '4', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0',
-            'intro');
-        // Add field introformat.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define field timecreated to be added to resourcelib.
-        //$table = new xmldb_table('resourcelib');
-        $field = new xmldb_field('timecreated', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'introformat');
-        // Add field timecreated.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define field timemodified to be added to resourcelib.
-        //$table = new xmldb_table('resourcelib');
-        $field = new xmldb_field('timemodified', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0', 'timecreated');
-
-        // Add field timemodified.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Define index course (not unique) to be added to resourcelib.
-        //$table = new xmldb_table('resourcelib');
-        $index = new xmldb_index('courseindex', XMLDB_INDEX_NOTUNIQUE, array('course'));
-
-        // Add index to course field.
-        if (!$dbman->index_exists($table, $index)) {
-            $dbman->add_index($table, $index);
-        }
-
         // Another save point reached.
         upgrade_mod_savepoint(true, 2014112403, 'resourcelib');
-    }
+    }*/
 
     /*
      * And that's all. Please, examine and understand the 3 example blocks above. Also
