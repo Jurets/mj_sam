f65dc070be90fd0370d8e0712ec4dd4b08e781b9
Jurets <jurets75@gmail.com>
2015-01-09 11:59:14 +0200

    Video Plugin, feedback * The two items on the settings page can actually be one setting

--- mod/videoresource/settings.php
+++ mod/videoresource/settings.php
@@ -20,11 +20,8 @@ if ($ADMIN->fulltree) {
                                    RESOURCELIB_DISPLAY_POPUP,
                                   );
                                   
-                                  
+    // settings items                              
     $settings->add(new admin_setting_heading('index_video', get_string('index_video', 'videoresource'), get_string('index_video_desc', 'videoresource', $CFG->wwwroot.'/mod/videoresource/video.php?action=index')));
-    $settings->add(new admin_setting_heading('add_video', get_string('add_video', 'videoresource'), get_string('add_video_desc', 'videoresource', $CFG->wwwroot.'/mod/videoresource/video.php?action=add')));
-    //$settings->add(new admin_setting_heading('sectionconfig', get_string('manage_sections', 'resourcelib'), get_string('manage_sections_desc', 'resourcelib', $CFG->wwwroot.'/mod/resourcelib/sections.php')));
-    //$settings->add(new admin_setting_heading('listconfig', get_string('manage_lists', 'resourcelib'), get_string('manage_lists_desc', 'resourcelib', $CFG->wwwroot.'/mod/resourcelib/lists.php')));
-
+    //$settings->add(new admin_setting_heading('add_video', get_string('add_video', 'videoresource'), get_string('add_video_desc', 'videoresource', $CFG->wwwroot.'/mod/videoresource/video.php?action=add')));
 }  
 ?>
